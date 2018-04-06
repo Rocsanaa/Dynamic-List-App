968f6e5248acb4ef6e759869aff6cf740b7ae83d
Rocsana Deleon <31667376+Rocsanaa@users.noreply.github.com>
2018-04-05 23:57:35 -0700

    Finished

--- dynamic_list.js
+++ dynamic_list.js
@@ -1,8 +1,20 @@
-var button = document.getElementById('Additem');
+var button = document.getElementById('inputItem');
+var ul = document.getElementById('dots');
 
-function Additem(){
-	var =
+function runresponse(){
+	var storeinput = document.getElementById('inputitem').value;
+    var item = document.createElement('li');
+    item.textContent = storeinput;
+    ul.appendChild(item);
+    var buttonE = document.createElement('button');
+    buttonE.textContent = "delete";
+    item.appendChild(buttonE);
 
+    function eliminate(){
+        buttonE.parentNode.remove();
 
-
+    }
+buttonE.addEventListener('click', eliminate);
 }
+
+button.addEventListener('click', runresponse);
